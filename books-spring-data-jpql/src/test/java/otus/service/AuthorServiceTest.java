9fe4aeb7628e550c913d4c52b9ccbcfe59956675
Ivan Kuznetsov <lamerinho@ya.ru>
2023-01-17 12:14:55 +0300

    change AuthorServiceTest

--- books-spring-data-jpql/src/test/java/otus/service/AuthorServiceTest.java
+++ books-spring-data-jpql/src/test/java/otus/service/AuthorServiceTest.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.convert.ConversionService;
-import ru.otus.config.ConversionServiceConfig;
-import ru.otus.converter.AuthorDtoToAuthorConverter;
-import ru.otus.converter.AuthorToAuthorDtoConverter;
 import ru.otus.domain.dto.AuthorDto;
 import ru.otus.domain.model.Author;
 import ru.otus.exception.AuthorNotFoundException;
@@ -22,12 +19,11 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = {AuthorServiceImpl.class, ConversionServiceConfig.class, AuthorDtoToAuthorConverter.class,
-        AuthorToAuthorDtoConverter.class})
+@SpringBootTest(classes = AuthorServiceImpl.class)
 class AuthorServiceTest {
     @Autowired
     private AuthorService authorService;
-    @Autowired
+    @MockBean
     private ConversionService conversionService;
     @MockBean
     private AuthorRepository authorRepository;
@@ -48,8 +44,10 @@ class AuthorServiceTest {
         AuthorDto authorDto = new AuthorDto("New author");
         Author author = new Author(authorDto.getFullName());
         Author savedAuthor = new Author(id, author.getFullName());
-        when(authorRepository.save(author)).thenReturn(savedAuthor);
         AuthorDto expectedAuthorDto = new AuthorDto(id, savedAuthor.getFullName());
+        when(authorRepository.save(author)).thenReturn(savedAuthor);
+        when(conversionService.convert(authorDto, Author.class)).thenReturn(author);
+        when(conversionService.convert(savedAuthor, AuthorDto.class)).thenReturn(expectedAuthorDto);
         AuthorDto actualAuthorDto = authorService.saveAuthor(authorDto);
         assertThat(actualAuthorDto).isEqualTo(expectedAuthorDto);
     }
@@ -60,9 +58,9 @@ class AuthorServiceTest {
         long id = 1;
         AuthorDto authorDto = new AuthorDto(id, "Edited author");
         Author author = new Author(id, authorDto.getFullName());
-        when(authorRepository.existsById(id)).thenReturn(true);
-        when(authorRepository.update(author)).thenReturn(author);
         AuthorDto expectedAuthorDto = new AuthorDto(id, author.getFullName());
+        when(authorRepository.findById(id)).thenReturn(Optional.of(author));
+        when(conversionService.convert(author, AuthorDto.class)).thenReturn(expectedAuthorDto);
         AuthorDto actualAuthorDto = authorService.updateAuthor(authorDto);
         assertThat(actualAuthorDto).isEqualTo(expectedAuthorDto);
     }
@@ -94,23 +92,45 @@ class AuthorServiceTest {
         assertThat(authorIsExist).isFalse();
     }
 
-    @DisplayName("Should find author")
+    @DisplayName("Should find author by id")
     @Test
-    void shouldFindAuthor() {
+    void shouldFindAuthorById() {
         long id = 1;
         Author author = new Author(id, "Test author");
-        when(authorRepository.findById(id)).thenReturn(Optional.of(author));
         AuthorDto expectedAuthorDto = new AuthorDto(id, author.getFullName());
+        when(authorRepository.findById(id)).thenReturn(Optional.of(author));
+        when(conversionService.convert(author, AuthorDto.class)).thenReturn(expectedAuthorDto);
         AuthorDto actualAuthorDto = authorService.findAuthorById(id);
         assertThat(actualAuthorDto).isEqualTo(expectedAuthorDto);
     }
 
-    @DisplayName("Should throw exception when try find not existing author")
+    @DisplayName("Should throw exception when try find not existing author by id")
     @Test
-    void shouldThrowExceptionWhenTryFindNotExistingAuthor() {
+    void shouldThrowExceptionWhenTryFindNotExistingAuthorById() {
         long id = 1;
         when(authorRepository.findById(id)).thenReturn(Optional.empty());
         assertThatThrownBy(() -> authorService.findAuthorById(id)).isInstanceOf(AuthorNotFoundException.class);
+    }
+
+    @DisplayName("Should find author by full name")
+    @Test
+    void shouldFindAuthorByFullName() {
+        long id = 1;
+        String fullName = "Test author";
+        Author author = new Author(id, fullName);
+        AuthorDto expectedAuthorDto = new AuthorDto(id, fullName);
+        when(authorRepository.findByFullName(fullName)).thenReturn(Optional.of(author));
+        when(conversionService.convert(author, AuthorDto.class)).thenReturn(expectedAuthorDto);
+        AuthorDto actualAuthorDto = authorService.findAuthorByFullName(fullName);
+        assertThat(actualAuthorDto).isEqualTo(expectedAuthorDto);
+    }
+
+    @DisplayName("Should throw exception when try find not existing author by full name")
+    @Test
+    void shouldThrowExceptionWhenTryFindNotExistingAuthorByFullName() {
+        String fullName = "Test author";
+        when(authorRepository.findByFullName(fullName)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> authorService.findAuthorByFullName(fullName)).isInstanceOf(AuthorNotFoundException.class);
     }
 
     @DisplayName("Should find all authors")
@@ -119,11 +139,15 @@ class AuthorServiceTest {
         Author author1 = new Author(1, "Test author 1");
         Author author2 = new Author(2, "Test author 2");
         Author author3 = new Author(3, "Test author 3");
-        when(authorRepository.findAll()).thenReturn(List.of(author1, author2, author3));
         AuthorDto authorDto1 = new AuthorDto(author1.getId(), author1.getFullName());
         AuthorDto authorDto2 = new AuthorDto(author2.getId(), author2.getFullName());
         AuthorDto authorDto3 = new AuthorDto(author3.getId(), author3.getFullName());
+        List<Author> authors = List.of(author1, author2, author3);
         List<AuthorDto> expectedAuthorDtoList = List.of(authorDto1, authorDto2, authorDto3);
+        when(authorRepository.findAll()).thenReturn(authors);
+        when(conversionService.convert(author1, AuthorDto.class)).thenReturn(authorDto1);
+        when(conversionService.convert(author2, AuthorDto.class)).thenReturn(authorDto2);
+        when(conversionService.convert(author3, AuthorDto.class)).thenReturn(authorDto3);
         List<AuthorDto> actualAuthorDtoList = authorService.findAllAuthors();
         assertThat(actualAuthorDtoList).isEqualTo(expectedAuthorDtoList);
     }
@@ -131,6 +155,17 @@ class AuthorServiceTest {
     @DisplayName("Should delete author")
     @Test
     void shouldDeleteAuthor() {
-        assertThatCode(() -> authorService.deleteAuthorById(1)).doesNotThrowAnyException();
+        long id = 1;
+        Author author = new Author(id, "Test author");
+        when(authorRepository.findById(id)).thenReturn(Optional.of(author));
+        assertThatCode(() -> authorService.deleteAuthorById(id)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Should throw exception when try delete author")
+    @Test
+    void shouldThrowExceptionWhenTryDeleteAuthor() {
+        long id = 1;
+        when(authorRepository.findById(id)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> authorService.deleteAuthorById(id)).isInstanceOf(AuthorNotFoundException.class);
     }
 }

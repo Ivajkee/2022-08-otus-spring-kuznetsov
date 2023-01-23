c399968135593ed7819b38dff3b9e48091d4fc32
Ivan Kuznetsov <lamerinho@ya.ru>
2023-01-23 21:15:35 +0300

    added type to @EntityGraph

--- books-spring-data-jpa/src/main/java/ru/otus/repository/BookRepository.java
+++ books-spring-data-jpa/src/main/java/ru/otus/repository/BookRepository.java
@@ -12,7 +12,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("select b from Book b left join fetch b.authors left join fetch b.genres where b.id = :id")
     Optional<Book> findByIdWithInfo(long id);
 
-    @EntityGraph(attributePaths = {"authors", "genres"})
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = {"authors", "genres"})
     Optional<Book> findByTitleIgnoreCase(String title);
 
     @Query("select b from Book b left join fetch b.authors left join fetch b.genres")

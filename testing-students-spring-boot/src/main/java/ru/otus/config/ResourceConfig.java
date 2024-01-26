c407db95c2b635dd2353921646359d52a6750c7d
Ivan Kuznetsov <lamerinho@ya.ru>
2024-01-26 19:14:15 +0300

    update sb testing-students-spring-boot

--- testing-students-spring-boot/src/main/java/ru/otus/config/ResourceConfig.java
+++ testing-students-spring-boot/src/main/java/ru/otus/config/ResourceConfig.java
@@ -2,14 +2,12 @@ package ru.otus.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Locale;
 import java.util.Map;
 
 @RequiredArgsConstructor
-@ConstructorBinding
 @ConfigurationProperties("resource.config")
 public class ResourceConfig {
     private static final String DEFAULT_CSV_URL = "static/questions/questions.csv";
